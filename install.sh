45e6462a2ec6e062df6d1acbcebacc5128012808
Fitz Elliott <fitz@cos.io>
2019-03-12 22:31:51 -0400

    install: build images after copying config
     * docker-compose won't run until all config files are in place.

--- install.sh
+++ install.sh
@@ -3,16 +3,15 @@
 git submodule init
 git submodule update
 
-for i in isp, experimenter, jamdb
-do
-    pushd $i
-    docker build -t $i:develop .
-    popd
-done
-
 cp config/jamdb/jam/settings/local.yml jamdb/jam/settings/
 cp config/jam-setup/config/local.yml jam-setup/config/
 cp config/experimenter/.env experimenter/
 cp config/isp/.env isp/
 
+for i in jamdb, jam-setup, experimenter, isp
+do
+    pushd $i
+    docker build -t $i:develop .
+    popd
+done
 
